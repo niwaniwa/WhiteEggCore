b53c879f98ef876b6f8a68653157e723fca3bba6
niwaniwa <konya.kokekokko@gmail.com>
2016-01-30 16:03:34 +0900

    commit

--- src/main/java/com/github/niwaniwa/we/core/api/callback/Callback.java
+++ src/main/java/com/github/niwaniwa/we/core/api/callback/Callback.java
@@ -6,7 +6,7 @@ public class Callback {
 	 * Twitter関連クラスで呼び出されるコールバックメソッド
 	 * @param twitter 成功したか
 	 */
-	public void onTwitter(Boolean twitter){ throw new UnsupportedOperationException(getClass().getSimpleName()); }
+	public void onTwitter(boolean twitter){ throw new UnsupportedOperationException(getClass().getSimpleName()); }
 
 	/**
 	 * その他で呼び出されるコールバックメソッド
