361da4edcc9d270a123372c52b42bc03ef2609eb
niwaniwa <konya.kokekokko@gmail.com>
2016-02-15 03:05:25 +0900

    add debug code

--- src/main/java/com/github/niwaniwa/we/core/listener/Debug.java
+++ src/main/java/com/github/niwaniwa/we/core/listener/Debug.java
@@ -1,23 +1,70 @@
 package com.github.niwaniwa.we.core.listener;
 
-import org.bson.Document;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.util.CachedServerIcon;
 
 import com.github.niwaniwa.we.core.WhiteEggCore;
 import com.github.niwaniwa.we.core.api.WhiteEggAPI;
-import com.github.niwaniwa.we.core.database.mongodb.MongoDataBaseCollection;
-import com.github.niwaniwa.we.core.database.mongodb.MongoDataBaseManager;
-import com.github.niwaniwa.we.core.database.mongodb.MongoDataBaseManager.MongoDataBase;
+import com.github.niwaniwa.we.core.command.abs.WhiteEggCommandExecutor;
 import com.github.niwaniwa.we.core.event.WhiteEggPostTweetEvent;
 import com.github.niwaniwa.we.core.event.WhiteEggPreTweetEvent;
 import com.github.niwaniwa.we.core.player.WhitePlayer;
+import com.github.niwaniwa.we.core.util.command.CommandFactory;
 
 import twitter4j.TwitterException;
 
 public class Debug implements Listener {
+
+	private CachedServerIcon icon;
+
+	public Debug() {
+		URL imageUrl = null;
+		InputStream input = null;
+		try {
+			imageUrl = new URL("https://minotar.net/helm/KokekoKko_");
+			input = imageUrl.openConnection().getInputStream();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		BufferedImage bufferedImage ;
+		CachedServerIcon icon = null;
+		try {
+			bufferedImage = ImageIO.read(input);
+			Image tmp = bufferedImage.getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+			BufferedImage dimg = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+
+			Graphics2D g2d = dimg.createGraphics();
+			g2d.drawImage(tmp, 0, 0, null);
+			g2d.dispose();
+			icon = Bukkit.getServer().loadServerIcon(dimg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(icon != null){ this.icon = icon; }
+		icon = null;
+	}
+
+	@EventHandler
+	public void onPing(ServerListPingEvent event){
+		if(icon == null){ return; }
+		event.setServerIcon(icon);
+	}
 
 	@EventHandler(priority = EventPriority.LOW)
 	public void onJoin(PlayerJoinEvent event) {
@@ -27,19 +74,23 @@ public class Debug implements Listener {
 			WhiteEggCore.getInstance().getLogger().info(
 					"-- " + player.getPlayer().getName() + "Join the game. --");
 		}
-		// database start
-		MongoDataBaseManager manager = new MongoDataBaseManager("192.168.99.100", 32768);
-		MongoDataBase database = manager.getDatabase("WhiteEgg");
-		MongoDataBaseCollection collection = new MongoDataBaseCollection(manager, database, "player");
-		Document playerData = new Document().append("player", new Document(player.serialize())).append("uuid", player.getUniqueId().toString());
-		if(collection.get(playerData, "uuid") != null){
-			Document uuid = new Document().append("uuid", player.getUniqueId().toString());
-			Document updatedoc = new Document("$addToSet", playerData);
-			collection.update(uuid, updatedoc);
-			return;
-		}
-		collection.getCollection().insertOne(playerData);
-		// database end
+		CommandFactory commandFactory = new CommandFactory(WhiteEggCore.getInstance(), "debug");
+		commandFactory.setExecutor(new WhiteEggCommandExecutor() {
+			@Override
+			public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+				sender.sendMessage("test");
+				return true;
+			}
+			@Override
+			public List<String> getUsing() { return null; }
+
+			@Override
+			public String getPermission() { return "whiteegg.core.command.debug"; }
+
+			@Override
+			public String getCommandName() { return "debug"; }
+		});
+		commandFactory.register();
 	}
 
 	@EventHandler
