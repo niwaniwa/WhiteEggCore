e02810dca10b93160b25199cce5e77e702694d4a
niwaniwa <konya.kokekokko@gmail.com>
2016-02-15 02:46:23 +0900

    Added twitter class
    - Twitterへのツイート、画像アップロードクラスの追加

--- src/main/java/com/github/niwaniwa/we/core/twitter/TweetScheduler.java
+++ src/main/java/com/github/niwaniwa/we/core/twitter/TweetScheduler.java
@@ -1,20 +1,12 @@
 package com.github.niwaniwa.we.core.twitter;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import javax.imageio.ImageIO;
 
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.github.niwaniwa.we.core.WhiteEggCore;
 import com.github.niwaniwa.we.core.api.callback.Callback;
 import com.github.niwaniwa.we.core.event.WhiteEggPostTweetEvent;
 import com.github.niwaniwa.we.core.event.WhiteEggPreTweetEvent;
@@ -22,222 +14,97 @@ import com.github.niwaniwa.we.core.util.Util;
 
 import twitter4j.Status;
 import twitter4j.StatusUpdate;
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.UploadedMedia;
 
 /**
- * ツイート送信クラス(使い捨て)
- * @author niwaniwa
+ * ツイートの投稿クラス
+ * @author mmott
  *
  */
 public class TweetScheduler extends BukkitRunnable {
 
-	private static final Pattern urlPattern =  Pattern.compile("(http://|https://){1}[\\w\\.\\-/:\\#\\?\\=\\&\\;\\%\\~\\+]+",
-			Pattern.CASE_INSENSITIVE);
-	private static final File path = new File(WhiteEggCore.getInstance().getDataFolder() + "/temp/");
-
-	private TwitterManager twitter;
-	private Callback callback;
+	private TwitterManager manager;
 	private String tweet;
-	private List<String> url = new ArrayList<>();
-	private List<Status> status = new ArrayList<>();
-	private List<File> medias = new ArrayList<>();
-	private boolean useMedia;
-	private int wait; //
+	private List<Callback> callback;
 
 	/**
 	 * コンストラクター
-	 * @param twitter TwitterManager
+	 * @param manager TwitterManager
 	 * @param tweet ツイート
-	 * @param wait 待機時間
 	 */
-	public TweetScheduler(TwitterManager twitter, String tweet, int wait){
-		this.twitter = twitter;
-		checkURL(tweet);
-		useMedia = !url.isEmpty();
-		this.wait = wait;
+	public TweetScheduler(TwitterManager manager, String tweet) {
+		this.manager = manager;
+		this.tweet = tweet;
+		this.callback = new ArrayList<>(0);
 	}
 
 	/**
 	 * コンストラクター
-	 * @param twitter TwitterManager
+	 * @param manager TwitterManager
 	 * @param tweet ツイート
-	 * @param wait 待機時間
-	 * @param callback ツイート後に呼び出す(戻り値はboolean)
+	 * @param callbacks ツイート後に実行する
 	 */
-	public TweetScheduler(TwitterManager twitter, String tweet, int wait, Callback callback){
-		this(twitter, tweet, wait);
-		this.callback = callback;
+	public TweetScheduler(TwitterManager manager, String tweet, Callback... callbacks) {
+		this.manager = manager;
+		this.tweet = tweet;
+		this.callback = Arrays.asList(callbacks);
 	}
 
-	/**
-	 * コンストラクター
-	 * @param twitter TwitterManager
-	 * @param tweet ツイート
-	 */
-	public TweetScheduler(TwitterManager twitter, String tweet){
-		this(twitter, tweet, 2);
+	public TwitterManager getManager() {
+		return manager;
 	}
 
-	/**
-	 * コンストラクター
-	 * @param twitter TwitterManager
-	 * @param tweet ツイート
-	 * @param callback ツイート後に呼び出す(戻り値はboolean)
-	 */
-	public TweetScheduler(TwitterManager twitter, String tweet, Callback callback){
-		this(twitter, tweet, 2);
-		this.callback = callback;
+	public String getTweet(){
+		return tweet;
 	}
 
-	private void checkURL(String tweet){
-		final Matcher matcher = urlPattern.matcher(tweet);
-		String toTweet = tweet;
+	public boolean tweet(){
+		if(!this.callPreEvent()){ return false; }
+		StatusUpdate statusUpdate = TwitterMediaUploader.checkMediaURL(tweet) == true ? null : new StatusUpdate(tweet);
+		if(statusUpdate == null){
+			List<String> urls = this.getURL();
+			statusUpdate = new StatusUpdate(tweet);
+			TwitterMediaUploader uploader = new TwitterMediaUploader(manager.getTwitter(), statusUpdate, urls);
+			uploader.upload();
+		}
+		Status status = null;
+		try {
+			status = manager.getTwitter().updateStatus(statusUpdate);
+		} catch (TwitterException e) { return false; }
+		WhiteEggPostTweetEvent postEvent = new WhiteEggPostTweetEvent(manager, status);
+		Util.callEvent(postEvent);
+		return true;
+	}
+
+	private List<String> getURL(){
+		List<String> result = new ArrayList<>();
+		final Matcher matcher = TwitterMediaUploader.urlPattern.matcher(tweet);
 		while (matcher.find()) {
 			String url = matcher.group();
-			if(checkExtension(url)){
-				toTweet = toTweet.replace(url, "");
-				this.url.add(url);
+			if(TwitterMediaUploader.checkMediaExtension(url)){
+				tweet.replace(url, "");
+				result.add(url);
 			}
 		}
-		this.tweet = toTweet;
+		return result;
 	}
 
-	@Override
-	public void run() {
-		callCallback(tweet());
-	}
-
-	private boolean tweet() {
-		Twitter t = twitter.getTwitter();
-		if(!twitter.check(tweet)){ return false; }
+	private boolean callPreEvent(){
 		WhiteEggPreTweetEvent preEvent;
-		if(twitter instanceof PlayerTwitterManager){
-			preEvent = new WhiteEggPreTweetEvent(((PlayerTwitterManager) twitter).getPlayer(), tweet);
+		if(manager instanceof PlayerTwitterManager){
+			preEvent = new WhiteEggPreTweetEvent(((PlayerTwitterManager) manager).getPlayer(), tweet);
 		} else {
 			preEvent = new WhiteEggPreTweetEvent(null, tweet);
 		}
 		Util.callEvent(preEvent);
 		if(preEvent.isCancelled()){ return false; }
 		tweet = preEvent.getTweet();
-		Status status = null;
-		try {
-			status = t.updateStatus(build());
-		} catch (TwitterException e) {}
-		delete();
-		if(status == null){ return false; }
-		this.status.add(status);
-		WhiteEggPostTweetEvent postEvent = new WhiteEggPostTweetEvent(twitter, status);
-		Util.callEvent(postEvent);
 		return true;
 	}
 
-	private void callCallback(Boolean b){
-		if(callback != null){ callback.onTwitter(b); }
-	}
-
-	private StatusUpdate build(){
-		StatusUpdate su = new StatusUpdate(tweet);
-		if(!useMedia){ return su; }
-		su.setMediaIds(uploadMedias());
-		return su;
-	}
-
-	private long[] uploadMedias(){
-		long[] mediaId = new long[this.url.size()];
-		for (int i = 0; i < url.size(); i++) {
-			File media = readImage(url.get(i));
-			medias.add(media);
-			mediaId[i] = uploadMedia(media);
-		}
-		return mediaId;
-	}
-
-	public Long uploadMedia(File path){
-		UploadedMedia media = null;
-		try {
-			media = twitter.getTwitter().uploadMedia(path);
-		} catch (TwitterException e) {}
-		return media.getMediaId();
-	}
-
-	private void delete(){
-		medias.stream().filter(f -> f.exists()).forEach(f -> f.delete());
-	}
-
-	public List<Status> getStatus() {
-		return status;
-	}
-
-	public void setStatus(List<Status> status) {
-		this.status = status;
-	}
-
-	public boolean isMedia() {
-		return useMedia;
-	}
-
-	public void setMedia(boolean media) {
-		this.useMedia = media;
-	}
-
-	public List<String> getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url.add(url);
-	}
-
-	public String getTweet() {
-		return tweet;
-	}
-
-	private File readImage(String url){
-		if(!path.exists()){ path.mkdirs(); }
-		InputStream input = responce(url);
-		if(input == null){ return null; }
-		String extension = url.split("\\.")[url.split("\\.").length - 1];
-		File imagePath = new File(path, UUID.randomUUID() + "." + extension);
-		BufferedImage image = null;
-		try {
-			image = ImageIO.read(input);
-			ImageIO.write(image, extension, imagePath);
-		} catch (Exception e) {}
-		return imagePath;
-	}
-
-	private InputStream responce(String url){
-		if(!checkExtension(url)){ return null; }
-		URL imageUrl = null;
-		InputStream input = null;
-		try {
-			imageUrl = new URL(url);
-			input = imageUrl.openConnection().getInputStream();
-		} catch (Exception e) {}
-		return input;
-	}
-
-	private boolean checkExtension(String url){
-		String[] format = new String[]{".jpg", ".gif", ".png", ".mp4"};
-		for(String s : format){
-			if(url.endsWith(s)){ return true; }
-			continue;
-		}
-		return false;
-	}
-
-	public int getWait() {
-		return wait;
-	}
-
-	public List<File> getMedia() {
-		return medias;
-	}
-
-	public void setWait(int wait) {
-		this.wait = wait;
+	@Override
+	public void run() {
+		callback.forEach(c -> c.onTwitter(tweet()));
 	}
 
 }
---
+++ src/main/java/com/github/niwaniwa/we/core/twitter/TwitterMediaUploader.java
@@ -0,0 +1,143 @@
+package com.github.niwaniwa.we.core.twitter;
+
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.UploadedMedia;
+
+/**
+ * Twitterへのメディアファイルアップロードクラス
+ * @author niwanwia
+ *
+ */
+public class TwitterMediaUploader {
+
+	public static final Pattern urlPattern =  Pattern.compile("(http://|https://){1}[\\w\\.\\-/:\\#\\?\\=\\&\\;\\%\\~\\+]+",
+			Pattern.CASE_INSENSITIVE);
+
+	private final Twitter twitter;
+	private final StatusUpdate update;
+	private final List<String> urls;
+
+	/**
+	 * コンストラクター
+	 * @param twitter Twitter{@link twitter4j.Twitter}
+	 * @param update StatsUpdate{@link twitter4j.StatusUpdate}
+	 * @param urls アップロードするメディアのURL
+	 */
+	public TwitterMediaUploader(Twitter twitter, StatusUpdate update, List<String> urls) {
+		this.twitter = twitter;
+		this.update = update;
+		this.urls = urls;
+	}
+
+	/**
+	 * StatsUpdateの取得
+	 * @return
+	 */
+	public StatusUpdate getUpdate() {
+		return update;
+	}
+
+	/**
+	 * URLの取得
+	 * @return
+	 */
+	public List<String> getUrls() {
+		return urls;
+	}
+
+	/**
+	 * メディアのアップロード
+	 */
+	public void upload(){
+		update.setMediaIds(uploadMedias());
+	}
+
+	private long[] uploadMedias(){
+		long[] mediaId = new long[this.urls.size()];
+		for (int i = 0; i < urls.size(); i++) {
+			InputStream media = responce(urls.get(i));
+			mediaId[i] = uploadMedia(UUID.randomUUID().toString(), media);
+		}
+		return mediaId;
+	}
+
+	/**
+	 * ファイルからメディアのアップロード
+	 * @param path
+	 * @return
+	 */
+	public long uploadMedia(File path){
+		UploadedMedia media = null;
+		try {
+			media = twitter.uploadMedia(path);
+		} catch (TwitterException e) {}
+		return media.getMediaId();
+	}
+
+	/**
+	 * InputStreamからメディアのアップロード
+	 * @param fileName File
+	 * @param stream stream
+	 * @return long
+	 */
+	public long uploadMedia(String fileName, InputStream stream){
+		UploadedMedia media = null;
+		try {
+			media = twitter.uploadMedia(fileName, stream);
+		} catch (TwitterException e) {}
+		return media.getMediaId();
+	}
+
+	private InputStream responce(String url){
+		if(!checkMediaExtension(url)){ return null; }
+		URL imageUrl = null;
+		InputStream input = null;
+		try {
+			imageUrl = new URL(url);
+			input = imageUrl.openConnection().getInputStream();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return input;
+	}
+
+	/**
+	 * 文字列にurlを含む確認します
+	 * @param string 対象の文字列
+	 * @return URLを含む場合true、それ以外の場合はfalseを返します
+	 */
+	public static boolean checkMediaURL(String string){
+		final Matcher matcher = urlPattern.matcher(string);
+		while (matcher.find()) {
+			String url = matcher.group();
+			if (checkMediaExtension(url)) { return true; }
+		}
+		return false;
+	}
+
+	/**
+	 * 文字列にTwitterが対応している拡張子を含んでいるか確認します(jpg gif png mp4)
+	 * @param url 対象の文字列
+	 * @return URLを含む場合true、それ以外の場合はfalseを返します
+	 */
+	public static boolean checkMediaExtension(String url){
+		String[] format = new String[]{".jpg", ".gif", ".png", ".mp4"};
+		for(String s : format){
+			if(url.endsWith(s)){ return true; }
+			continue;
+		}
+		return false;
+	}
+
+
+}
